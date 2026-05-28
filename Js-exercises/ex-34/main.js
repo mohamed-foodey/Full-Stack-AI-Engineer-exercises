a47b6b9f67bd3cb065e24c843e4ab99ea92e3e47
mohamed-foodey <foodeye2022@gmail.com>
2026-05-28 14:09:38 +0300

    added ex-34

---
+++ Js-exercises/ex-34/main.js
@@ -0,0 +1,16 @@
+const list= document.querySelector('.list')
+
+function addItem(){
+    const newItem = document.createElement('li')
+    newItem.textContent='new item'
+    list.appendChild(newItem)
+
+}
+
+
+function removeItem(){
+     if(list.lastChild){
+    list.removeChild(list.lastChild)
+     }
+
+}

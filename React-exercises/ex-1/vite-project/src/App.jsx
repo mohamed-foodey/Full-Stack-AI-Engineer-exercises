db98525016c44e86ce5e5c882bd9019caf01cae8
mohamed-foodey <foodeye2022@gmail.com>
2026-05-30 17:46:02 +0300

    exercise-1

---
+++ React-exercises/ex-1/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import UserCard from "./UserCard"
+
+function App() {
+  
+  
+  return (
+    <>
+    <UserCard/>
+   
+    
+    
+    </>
+    
+  )
+}
+
+export default App
---
+++ React-exercises/ex-1/vite-project/src/UserCard.jsx
@@ -0,0 +1,11 @@
+function UserCard(){
+    return(
+        <div>
+            <h2>Foodey</h2>
+            <p>I am a student</p>
+        </div>
+        
+        
+    )
+}
+export default UserCard
